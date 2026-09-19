f6862f241af28ae811841d44722c8fe232c62f85
BuiHoaBku97 <vanhoabku97@gmail.com>
2026-09-19 21:06:51 +0700

    - param

--- pharmacy-service/src/main/java/startup/vn/pharmacyservice/controllers/PharmacyController.java
+++ pharmacy-service/src/main/java/startup/vn/pharmacyservice/controllers/PharmacyController.java
@@ -1,5 +1,6 @@
 package startup.vn.pharmacyservice.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pharmacy")
 public class PharmacyController {
 
+    @Value("${app.brand-name}")
+    private String brandName;
+
+    @Value("${app.hotline}")
+    private String hotline;
+
     @GetMapping("/test")
     public String test() {
         return "Hello, Pharmacy Service!";
+    }
+
+    @GetMapping("/info")
+    public String getInfo() {
+        return "System info - Brand Name: " + brandName + ", Hotline: " + hotline;
     }
 }
